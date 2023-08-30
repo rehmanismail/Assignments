597cbf152ef94fffe0772f4853158d83c076401e
rehmanismail <139158527+rehmanismail@users.noreply.github.com>
2023-08-30 11:12:25 +0500

    Add files via upload

---
+++ Exercise No 2/index.js
@@ -0,0 +1,3 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person.
+var personName = 'Rehman Ismail';
+console.log("How are you ".concat(personName));
---
+++ Exercise No 2/index.ts
@@ -0,0 +1,3 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person.
+let personName: string = 'Rehman Ismail';
+console.log(`How are you ${personName}`);
