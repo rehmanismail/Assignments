bb2ce8f9c26699a3bae94815ceee929925dfbe11
rehmanismail <139158527+rehmanismail@users.noreply.github.com>
2023-08-30 11:20:03 +0500

    Add files via upload

---
+++ Exercise No 5/index.ts
@@ -0,0 +1,7 @@
+//Repeat Exercise 4, but this time store the famous person’s name in a variable 
+//called famous_person. Then compose your message and store it in a new variable
+ //called message. Print your message.
+let famousPersonName: string = 'Quaid e Azam';
+
+let message: string = famousPersonName + 'how are you';
+console.log(message);
